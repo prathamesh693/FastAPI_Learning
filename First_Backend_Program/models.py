e0071a48a27af0c4d7797688931a48eb80f1610f
Jadhav Prathamesh Suresh <prathameshj693@gmail.com>
2025-10-03 08:59:35 +0530

    Added comment in every line

--- First_Backend_Program/models.py
+++ First_Backend_Program/models.py
@@ -1,8 +1,12 @@
+# Import BaseModel from pydantic
+# Pydantic is used for data validation and parsing in FastAPI
+
 from pydantic import BaseModel
 
+# define a product model class inheriting from BaseModel
 class Product(BaseModel):
-    id: int
-    name: str
-    description: str
-    price: float
-    quantity: int
+    id: int # product ID as an integer
+    name: str # product name as a string
+    description: str # product description as a string
+    price: float # product price as a float
+    quantity: int # product quantity as an integer
